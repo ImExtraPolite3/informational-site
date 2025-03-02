b0bbdf7c73a7d8a7613e85cb1ba3c0bc60513013
ExtraPolite <imextrapolite2@gmail.com>
2025-03-02 13:14:40 -0500

    changes

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.url === '/') {
+      fs.readFile('index.html', function (err, data) {
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === '/about') {
+      fs.readFile('about.html', function (err, data) {
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === '/contact') {
+      fs.readFile('contact-me.html', (err, data) => {
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile('404.html', (err, data) => {
+        res.write(data);
+        return res.end();
+      });
+    }
+  })
+  .listen(8080);
