c1f7b8069737126fbeb93660d6f4ac34262b96c5
ExtraPolite <imextrapolite2@gmail.com>
2025-03-05 15:03:33 -0500

    get author id

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,8 @@
 const { Router } = require('express');
-
+const { getAuthorById } = require('../controllers/authorsController');
 const authorRouter = Router();
 
-authorRouter.get('/', (req, res) => res.send('all authors'));
-authorRouter.get('/:authors', (req, res) => {
-  const { authorId } = req.params;
-  res.send(`AuthorID; ${authorId}`);
-});
+authorRouter.get('/author', (req, res) => res.send('all routers'));
+authorRouter.get('/:authorId', getAuthorById);
 
 module.exports = authorRouter;
