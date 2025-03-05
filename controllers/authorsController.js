da1296e472cedd478732b9023697febbf5365960
ExtraPolite <imextrapolite2@gmail.com>
2025-03-05 17:23:26 -0500

    add try and catch to authorsController

--- controllers/authorsController.js
+++ controllers/authorsController.js
@@ -2,14 +2,20 @@ const db = require('../db');
 
 async function getAuthorById(req, res) {
   const { authorId } = req.params;
-  const author = await db.getAuthorById(Number(authorId));
 
-  if (!author) {
-    res.status(404).send('Author not found');
-    return;
+  try {
+    const author = await db.getAuthorById(Number(authorId));
+
+    if (!author) {
+      res.status(404).send('Author not found');
+      return;
+    }
+
+    res.send(`Author Name: ${author.name}`);
+  } catch {
+    console.error('error retrieving author', error);
+    res.status(500).send('Internal Server Error');
   }
-
-  res.send(`Author Name: ${author.name}`);
 }
 
 module.exports = { getAuthorById };
