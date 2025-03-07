df9aa9e3768539f0c5ea09bfd61f52ac9880eddc
ExtraPolite <imextrapolite2@gmail.com>
2025-03-06 23:10:56 -0500

    add view and work with it for a bit

--- app.js
+++ app.js
@@ -3,10 +3,37 @@ const app = express();
 const authorRouter = require('./routes/authorRouter');
 const bookRouter = require('./routes/bookRouter');
 const indexRouter = require('./routes/indexRouter');
+const path = require('node:path');
 
-app.use('/author', authorRouter);
-app.use('/books', bookRouter);
-app.use('/', indexRouter);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const links = [
+  { href: '/', text: 'Home' },
+  { href: 'about', text: 'About' },
+];
+
+const users = ['Rose', 'Cake', 'Biff'];
+
+app.get('/', (req, res) => {
+  res.render('index', { links: links, users: users });
+});
+
+// app.use('/author', authorRouter);
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).send(err);
+// });
+// app.use('/books', bookRouter);
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).send(err);
+// });
+// app.use('/', indexRouter);
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).send(err);
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => {
