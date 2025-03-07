25421ba2cf8ab6b0af06cef8c5968d8f14502b17
ExtraPolite <imextrapolite2@gmail.com>
2025-03-07 16:11:45 -0500

    add css to project

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const authorRouter = require('./routes/authorRouter');
 const bookRouter = require('./routes/bookRouter');
 const indexRouter = require('./routes/indexRouter');
 const path = require('node:path');
+const assetsPath = path.join(__dirname, 'public');
+
+app.use(express.static(assetsPath));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
